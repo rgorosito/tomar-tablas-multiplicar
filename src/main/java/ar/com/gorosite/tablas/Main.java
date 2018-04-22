036b8863b86ebd73564d450f9567a731cd0d765c
Ricardo A. Gorosito <rgorosito@gmail.com>
2018-04-22 10:38:26 -0300

    Para que no salga sin el enter y poder ver los resultados

--- src/main/java/ar/com/gorosite/tablas/Main.java
+++ src/main/java/ar/com/gorosite/tablas/Main.java
@@ -6,11 +6,11 @@ import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Main {
-	
+
 	private static Random random = new Random(System.currentTimeMillis());
 
 	public static void main(String[] args) {
-		
+
 		int cuantas=0;
 		long comienzo=0;
 		int respuestasCorrectas=0;
@@ -20,18 +20,18 @@ public class Main {
 
 			cuantas = pideNumeroNatural("¿Cuantas cuentas?", br);
 			int hastaQueNumero = pideNumeroNatural("¿Hasta qué número?", br);
-			
+
 			System.out.println("Ok... comenzamos...\n");
-			
+
 			comienzo = System.currentTimeMillis();
-			
+
 			for (int i=0; i<cuantas ; i++ ) {
-				
+
 				int x = numeroAleatorio(hastaQueNumero);
 				int y = numeroAleatorio(10);
 
 				int respuesta = pideNumeroNatural("¿Cuanto es ... "+x+" x "+y+" ?", br);
-				
+
 				if ( respuesta == x*y ) {
 					respuestasCorrectas++;
 					System.out.println("BIEN!");
@@ -39,36 +39,40 @@ public class Main {
 					respuestasIncorrectas++;
 					System.out.println("no :-(");
 				}
-				
+
 			}
+
+			long fin = System.currentTimeMillis();
+
+			long demoroSegundos = (fin-comienzo)/1000;
+
+			System.out.println("Terminamos!!!\n");
+			System.out.println("Respuestas correctas:"+respuestasCorrectas);
+			System.out.println("Respuestas incorrectas:"+respuestasIncorrectas);
+			System.out.println("Tiempo total en segundos:"+demoroSegundos);
+			System.out.println("Tiempo promedio por cuenta:"+demoroSegundos/cuantas);
+			System.out.println("\n\n");
+			System.out.println("Presione ENTER para finalizar");
+			br.read();
+
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		long fin = System.currentTimeMillis();
-		
-		long demoroSegundos = (fin-comienzo)/1000;
-		
-		System.out.println("Terminamos!!!\n");
-		System.out.println("Respuestas correctas:"+respuestasCorrectas);
-		System.out.println("Respuestas incorrectas:"+respuestasIncorrectas);
-		System.out.println("Tiempo total en segundos:"+demoroSegundos);
-		System.out.println("Tiempo promedio por cuenta:"+demoroSegundos/cuantas);
 
 	}
-	
+
 	private static int numeroAleatorio(int maximo) {
 		if ( maximo < 2 ) {
 			return 1;
 		}
 		return random.nextInt(maximo-1)+1;
 	}
-	
+
 	private static int pideNumeroNatural(String pregunta, BufferedReader br) throws IOException {
 
 		while ( true ) {
-			
+
 			System.out.println(pregunta);
 
 			try {
@@ -84,7 +88,7 @@ public class Main {
 			}
 		}
 	}
-	
+
 	private static void muestraMensajeError(String s) {
 		System.err.println(s);
 		try {
